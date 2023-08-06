4cc8eed57fb1d458425c4af96db624685fa9763b
Satendra Raj <satendra.raj@nium.com>
2023-08-06 20:49:19 +0530

    backend done

--- ai.js
+++ ai.js
@@ -1,19 +1,22 @@
-import axios from "axios";
+const axios = require("axios");
 
-export const make_intial_prompt = (players, attributes) => [
-    {
+const make_intial_prompt = (players, attributes) => {
+    return {
         "role": "system",
-        "content": `Act as a game api.\n\ntwo players ${players[0]} and ${players[1]} plays a game of life trying to kill each other.\n\nFirst ${players[0]} writes and ${players[1]} replies then ${players[1]} writes and ${players[0]} replies and so on.\n\nThere are attributes ${attributes.join(',')} each with initial being 100 and max 200 and min 0.\nAny attribute going to zero means that player dies.\n\nAll attributes are for you to decide that what happens on what action.\n\nMake sure everything follows a story failing of which send a response with valid as false other response should have valid as true. also give a reason when having valid as false.\n\nfor valid as true give current attribute values of both players\n\nYou can only reply as json object.if confused reply as valid as false and reason as the reason for confusion.\nvalid as true should also give an justification on why and how each attributes were changed and by how much think deep and create a story of 50 words.\n\nThere are only these json with which you can reply with\nfor valid:\n{\n\"valid\":true,\n\"reason\":\"Justification for changes\"\n\"data\":{\n    \"${players[0]}\": {\n    <attributes-data>\n  },\n  \"${players[1]}\": {\n    <attributes-data>\n  }\n  }\n}\nfor invalid:\n{\n\"valid\":false,\n\"reason\":\"Reason for invalid response\"\n}\n\n`
+        "content": `Act as a game api.\n\ntwo players ${players[0]} and ${players[1]} plays a game of life trying to kill each other.\n\nFirst ${players[0]} writes and ${players[1]} replies then ${players[1]} writes and ${players[0]} replies and so on.\n\nThere are attributes ${attributes.join(',')} each with initial being 10 and max 15 and min 0.\nAny attribute going to zero means that player dies.\n\nAll attributes are for you to decide that what happens on what action.\n\nMake sure everything follows a story failing of which send a response with valid as false other response should have valid as true. also give a reason when having valid as false.\n\nfor valid as true give current attribute values of both players\n\nYou can only reply as json object.if confused reply as valid as false and reason as the reason for confusion.\nvalid as true should also give an justification on why and how each attributes were changed and by how much think deep and create a story of 50 words.\n\nThere are only these json with which you can reply with\nfor valid:\n{\n\"valid\":true,\n\"reason\":\"Justification for changes\"\n\"data\":{\n    \"${players[0]}\": {\n    <attributes-data>\n  },\n  \"${players[1]}\": {\n    <attributes-data>\n  }\n  }\n}\nfor invalid:\n{\n\"valid\":false,\n\"reason\":\"Reason for invalid response\"\n}\n\n donot format the json\n`
     }
-];
+};
 
-export const make_promt = (challenger, replyer, challenger_message, replyer_message) => {
-    return [
-        { role: "user", content: `${challenger}:${challenger_message} \n ${replyer}:${replyer_message}` }
-    ];
+const make_promt = (challenger, replyer, challenger_message, replyer_message) => {
+    return { role: "user", content: `${challenger}:${challenger_message} \n ${replyer}:${replyer_message}` };
 }
 
-export const get_new_state_update_prompt = async (prompts) => {
+const get_new_state_update_prompt = async (prompts, propts_sv, tries = 0) => {
+    // remove extra values _id from prompts
+    prompts.forEach((prompt) => {
+        delete prompt._id;
+    });
+
     const headers = {
         'Content-Type': 'application/json',
         'Authorization': 'Bearer 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'
@@ -27,8 +30,26 @@ export const get_new_state_update_prompt = async (prompts) => {
         "frequency_penalty": 0,
         "presence_penalty": 0
     };
-    const res = await axios.post('https://api.openai.com/v1/chat/completions', data, { headers: headers });
-    const AI_prompt =  res.data.choices[0].message;
-    prompts.push(AI_prompt);
-    return JSON.parse(AI_prompt.content);
+    // try three times to get a non error response
+    try {
+        const res = await axios.post('https://api.openai.com/v1/chat/completions', data, { headers: headers });
+        const AI_prompt = res.data.choices[0].message;
+        const  resData = JSON.parse(AI_prompt.content);
+        propts_sv.push(AI_prompt);
+        return resData;
+    } catch (error) {
+        if (tries >= 3) {
+            console.log(error);
+            return null;
+        }
+        return await get_new_state_update_prompt(prompts, tries + 1);
+    }
+    
 }
+
+//export
+module.exports = {
+    make_intial_prompt,
+    make_promt,
+    get_new_state_update_prompt
+};
--- room.js
+++ room.js
@@ -1,24 +1,72 @@
 const mongoose = require('mongoose');
 
+const attributeSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  value: { type: Number, required: true },
+});
+
+const playerSchema = new mongoose.Schema({
+  player: { type: String, required: true },
+  attributes: [attributeSchema],
+});
+
+const promptSchema = new mongoose.Schema({
+    role: { type: String, required: true },
+    content: { type: String, required: true },
+});
+
 const roomSchema = new mongoose.Schema({
-    _id: { type: String, required: true },
-    attr: {
-        type: Map,
-        of: new mongoose.Schema({}, { strict: false }),
-        required: true,
-    },
-    players: [{ type: String, required: true }],
-    prompts: [{
-        role: { type: String, required: true },
-        content: { type: String, required: true },
-    }],
-    current_round: {
-        challenger: { type: String, required: true },
-        replier: { type: String, required: true },
-        challenger_message: { type: String, required: true },
-    }
+  _id: { type: String, required: true },
+  attr: [playerSchema],
+  players: [{ type: String, required: true }],
+  prompts: [promptSchema],
+  current_round: {
+    challenger: { type: String, required: true },
+    replier: { type: String, required: false },
+    challenger_message: { type: String, required: false },
+  },
 });
 
 const Room = mongoose.model('Room', roomSchema);
 
 module.exports = Room;
+
+const sc = {
+    _id: "rooms2323",
+    attr: [
+        {
+            player: "satendra",
+            attributes: [
+                { name: "name", value: 100 },
+                { name: "name2", value: 100 },
+                { name: "name3", value: 100 },
+                { name: "name4", value: 100 }
+            ]
+        },
+        {
+            player: "sauravh",
+            attributes: [
+                { name: "name", value: 100 },
+                { name: "name2", value: 100 },
+                { name: "name3", value: 100 },
+                { name: "name4", value: 100 }
+            ]
+        }
+    ],
+    players: ["satendra", "saurabh"],
+    prompts: [
+        {
+            "role": "system",
+            "content": "you are ai"
+        },
+        {
+            "role": "user",
+            "content": "user message"
+        }
+    ],
+    current_round: {
+        challenger: "satendra",
+        replier: "saurabh",
+        challenger_message: "challenger message",
+    }
+}
